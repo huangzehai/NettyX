64c416196dcd4648140602d3a36265e432c179f8
AdamHuang <chaogeek@foxmail.com>
2016-02-26 16:28:46 +0800

    Speaking in POJO instead of ByteBuf

---
+++ src/main/java/com/hzh/nettyx/time/TimeDecoder.java
@@ -0,0 +1,20 @@
+package com.hzh.nettyx.time;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * Created by Adam on 2016/2/26.
+ */
+public class TimeDecoder extends ByteToMessageDecoder { // (1)
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) { // (2)
+        if (in.readableBytes() < 4) {
+            return; // (3)
+        }
+        out.add(new UnixTime(in.readUnsignedInt()));
+    }
+}
